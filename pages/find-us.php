95fe6f428eb48319e7516c6b37279dbe0f18999e
jaxhusen <jaxhusen@hotmail.com>
2023-01-24 09:20:22 +0100

    hitta hit

--- pages/find-us.php
+++ pages/find-us.php
@@ -1,23 +1,72 @@
 <?php
-$office = "Gran Canaria, youmbo center";
+ $office = "Gran Canaria, youmbo center";
 require_once __DIR__ . "/../classes/Template.php";
 require_once __DIR__ . "/../classes/UsersDb.php";
+
 
 Template::header("Hitta hit");
 ?>
 <?php
-    if(isset($_POST["submit_adress"])){
-        $adress = $_POST["adress"]; ?>
-<iframe class="map" src="https://www.google.com/maps?q=<?php echo $office;?>&output=embed"></iframe>
-<?php
-    }
+
+$lat2 = 58.933333;
+$long2 = 17.9;
+
+function calculateDistance($lat1, $long1, $lat2, $long2){
+    $first = $long1 - $long2;
+    $distance = (sin(deg2rad($lat1))) * 
+              sin(deg2rad($lat2)) + 
+             (cos(deg2rad($lat1)) * 
+              cos(deg2rad($lat2)) *
+              cos(deg2rad($first)));
+    $distance = acos($distance);
+    $distance = rad2deg($distance);
+    $distance = $distance * 1.609344; 
+
+    return $distance;
+}
+
+
+if(isset($_POST["submit_coordinates"])){
+    $latitude = $_POST["latitude"]; 
+    $longitude = $_POST["longitude"];
+   var_dump(calculateDistance($latitude, $longitude, $lat2, $long2));  
+}
+
 ?>
+
+
+<h2 class="admin-heading"> Long / Lat</h2>
+<h4> Avståndet mellan <?=$latitude ?>, <?= $longitude ?> och Youmbo Centre, Gran Canaria är: <?= $distance ?> </h4>
+<?php var_dump($distance);  ?>
+
+
     <form method="POST">
-        <p>
-            <input type="text" name="adress" placeholder="Skriv in din adress">
-        </p>
-            <input type="submit" name="submit_adress">
-    </form>
+            <input type="text" name="latitude" placeholder="Skriv in latitude">
+            <input type="text" name="longitude" placeholder="Skriv in longitude">
+            <input type="submit" name="submit_coordinates">
+    </form> 
+ <iframe class="map" src="https://www.google.com/maps?q=<?php echo $office?>&output=embed"></iframe>
+    <?php
+
+?>
+
+
+
+
+
+
+
+<!-- <iframe class="map" src="https://www.google.com/maps?q=<?php echo $office;?>&output=embed"></iframe>
+ -->
+
+
+
+
+
+
+
+
+
 
  
 
